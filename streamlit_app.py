4bea5b5e89b1ba56de56734c9e9dc09e801115f1
lucy Taylor <104491878+lptaylor-nhbc@users.noreply.github.com>
2022-08-03 15:47:05 +0100

    Update streamlit_app.py
    changed second title to header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Bluberry Oatmeal')
 
